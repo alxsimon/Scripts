9dbae5f3bee737867603064f7ee1e988adddf4c3
alxsimon <alexis.simon@normalesup.org>
2018-12-12 17:45:06 +0100

    New script to run CLUMPAK analysis

---
+++ run_clumpak.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+WD=$1
+TYPE=$2
+
+if [ $TYPE = 'admixture' ]; then
+	ZIPFILE=$(find ${WD} -name '*_Q.zip')
+	POPFILE=$(find ${WD} -name '*clumpak_pop')
+else
+	ZIPFILE=$(find ${WD} -name '*.zip')
+fi
+
+cd /Applications/CLUMPAK
+
+if [ $TYPE = 'admixture' ]; then
+	perl CLUMPAK.pl --id 1 --dir ${WD}/tmp --file ${ZIPFILE} --inputtype ${TYPE} --podtopop ${POPFILE}
+else
+	perl CLUMPAK.pl --id 1 --dir ${WD}/tmp --file ${ZIPFILE} --inputtype ${TYPE}
+fi
+
+unzip ${WD}/tmp/1.zip -d ${WD}/
+mv ${WD}/1 ${WD}/CLUMPAK
+rm -r ${WD}/tmp
